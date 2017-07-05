d145a33a5549843ccf483b340cd6e036aa3edb97
duxiaodong <duxiaodong@darlin.me>
2017-07-06 00:34:52 +0800

    complete add test

--- src/base/add/index.spec.ts
+++ src/base/add/index.spec.ts
@@ -2,8 +2,9 @@ import add from '.'
 
 describe('addInt', () => {
   test('normal', () => {
+    expect(add('1')).toBe('1')
     expect(add('1', '1')).toBe('2')
-    expect(add('1', '1', '1')).toBe('3')
+    expect(add('.1', '1', '1')).toBe('2.1')
     expect(add('1', '1', '-1.8')).toBe('0.2')
   })
 })
