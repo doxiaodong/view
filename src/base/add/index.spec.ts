2463ce432b8228b6199d5b19c90f4ff3a5b3c827
duxiaodong <duxiaodong@darlin.me>
2017-07-06 11:17:17 +0800

    add args check

--- src/base/add/index.spec.ts
+++ src/base/add/index.spec.ts
@@ -2,9 +2,28 @@ import add from '.'
 
 describe('addInt', () => {
   test('normal', () => {
+    expect(add()).toBe('0')
     expect(add('1')).toBe('1')
     expect(add('1', '1')).toBe('2')
     expect(add('.1', '1', '1')).toBe('2.1')
     expect(add('1', '1', '-1.8')).toBe('0.2')
+  })
+
+  test('args', () => {
+    expect(
+      () => {
+        add(1 as any)
+      }
+    ).toThrowError('args must be number like string')
+    expect(
+      () => {
+        add('asd')
+      }
+    ).toThrowError('args must be number like string')
+    expect(
+      () => {
+        add('1.1.2')
+      }
+    ).toThrowError('args must be number like string')
   })
 })
--- src/base/add/index.ts
+++ src/base/add/index.ts
@@ -1,7 +1,19 @@
 import { addFloat } from './float'
 
 function add(...args: string[]) {
-  // TODO: check args isNamberString[]
+  const isArgsInvalid = args.some((arg) => {
+    if (typeof arg !== 'string') {
+      return true
+    }
+    if (isNaN(+arg)) {
+      return true
+    }
+  })
+
+  if (isArgsInvalid) {
+    throw new Error('args must be number like string')
+  }
+
   return args.reduce((sum, value) => addFloat(sum, value), '0')
 }
 
